7a4d47fb78bd81c7049aecbfb37cd4fcbdc3d469
PalguniBR <126310032+PalguniBR@users.noreply.github.com>
2023-02-25 21:40:56 +0530

    Add files via upload

---
+++ calculator/pratice.js
@@ -0,0 +1,23 @@
+let result = document.getElementById("inputext")
+
+let calculator=(Number)=>{
+       result.value+=Number;
+}
+
+
+ let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+      catch(err){
+        alert("enter the valid input")
+      }
+ }
+
+ function clr(){
+    result.value=" ";
+ }
+
+ function del(){
+    result.value=result.value.slice(0 -1);
+ }
